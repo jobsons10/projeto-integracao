d8a94fb20da4059a47f2fc82db5abdb9d3593ef6
jobsons10 <jobsonsobrinho@grupootg.com>
2023-01-25 17:06:31 -0400

    a

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 fetch('https://api-grupootg.onrender.com/user')
 .then((res) => res.json())
 .then((data) => console.log(data))
+
+// Adicionar as class de animation na bola
+
+const ball = document.querySelector('#ball-img');
+
